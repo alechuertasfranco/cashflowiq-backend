1b4b794711c0edce00ffca5e9bb25a960596ba18
Alec Huertas <alec.huertas@janaq.com>
2026-03-27 17:19:18 -0500

    feat: set docker config

--- app/db/session.py
+++ app/db/session.py
@@ -1,8 +1,11 @@
 # app/db/session.py
 
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv(
     "DATABASE_URL", "postgresql://postgres:postgres@db:5432/cashflowiq"
