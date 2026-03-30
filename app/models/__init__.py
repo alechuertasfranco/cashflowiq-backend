5474ec2aabb7eab7c11ec2735c484ac227b5a96d
Alec Huertas <alec.huertas@janaq.com>
2026-03-30 14:17:16 -0500

    feat: use env on database config

---
+++ app/models/__init__.py
@@ -0,0 +1,11 @@
+# app/models/__init__.py
+
+from app.models.user import User
+from app.models.bank_entity import BankEntity
+from app.models.bank_account import BankAccount
+
+# 🔜 aquí luego agregas:
+# from app.models.credit_card import CreditCard
+# from app.models.credit_card_movement import CreditCardMovement
+# from app.models.investment_fund import InvestmentFund
+# from app.models.investment_movement import InvestmentMovement
