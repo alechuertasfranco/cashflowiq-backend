63d0fcbe629fd9f5e66eb29e416c151320ee83e7
Alec Huertas <alec.huertas@janaq.com>
2026-04-06 17:24:42 -0500

    feat: add categories routes

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ from app.api.routes import bank_entities
 from app.api.routes import currencies
 from app.api.routes import credit_cards
 from app.api.routes import investment_funds
+from app.api.routes import categories
 
 # Configuracion global de logging
 logging.basicConfig(
@@ -22,3 +23,4 @@ app.include_router(bank_entities.router)
 app.include_router(currencies.router)
 app.include_router(credit_cards.router)
 app.include_router(investment_funds.router)
+app.include_router(categories.router)
